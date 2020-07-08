7e9cf657a4860783b95c92709f8ec8a5769683f7
chaidrinker <info@indianmuslim.org.uk>
2020-07-08 11:37:30 +0100

    Change to Npgsql as works out of the box

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Npgsql;
+
+namespace adotimetravel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "User Id=timetraveller;Password=NH1b32thefree;Host=localhost;Database=timetravelmovies;Persist Security Info=True";
+
+            // Provide the query string with a parameter placemholder
+
+            string queryString = "SELECT movieid, name, releasedate, mainactor from movies order by releasedate desc";
+
+            // Result of code execution or number of lines?
+            int result = 0;
+            using (NpgsqlConnection pgConnection = new NpgsqlConnection(connectionString))
+            {
+                NpgsqlCommand command = new NpgsqlCommand(queryString, pgConnection);
+                
+                try
+                {
+                    pgConnection.Open();
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    pgConnection.Close();
+                }
+            }
+
+            Console.WriteLine("End of Program");
+        }
+    }
+}
